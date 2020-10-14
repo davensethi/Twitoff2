fb48f7b89b3dfae22581d6874abe330e47f721e9
davensethi@gmail.com <davensethi@gmail.com>
2020-10-13 21:33:31 -0400

    Assignment 2

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, render_template
 from .models import DB, User
+from .twitter import insert_example_users
+from os import getenv
 
 
 ## initalizing the app
@@ -11,14 +13,26 @@ def create_app ():
     """creates and configures flask application"""
     
     app = Flask(__name__)
-    app.config['SQLAlchemy_DATABASE_URI'] = "sqlite://db.sqlite3"
-    app.config['SQLAlchemy_TRACK_MODIFCATIONS'] = False
+    app.config['SQLAlCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+    app.config['SQLAlCHEMY_TRACK_MODIFCATIONS'] = False
     DB.init_app(app)
 
-    #ToDO -- make rest of the application
     @app.route('/')
     def root():
-        user = USer.query.all()
         return render_template("base.html", title="Home", users= User.query.all())
+    
+    @app.route('/update')
+    def update():
+        #adds our users
+        insert_example_users()
+        return render_template('base.html',title = "Home", users=User.query.all())
+    
+    @app.route('/reset')
+    def reset():
+    # resets datatbase
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', titel='Home')
+        
     
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -1,23 +1,39 @@
 """ SQLAlchemy models and utlity  funcitons for twittoff"""
 
+
 from flask_sqlalchemy import SQLAlchemy
+
 
 DB = SQLAlchemy()
 
+# User table with Columns id and name 
 class User(DB.Model):
     """Twitter Users corresponding to Tweets"""
     id = DB.Column(DB.BigInteger, primary_key=True)
     name = DB.Column(DB.String, nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
-    def __repre__(self):
+    def __repr__(self):
         return "<User: {}>".format(self.name)
 
+
+#Tweet table with column id, text, and user_id 
 class Tweet(DB.Model):
     """tweet related to a user"""
     id = DB.Column(DB.BigInteger,primary_key=True)
     text = DB.Column(DB.Unicode(300))
+    vect = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
     user =DB.relationship("User", backref=DB.backref("tweets", lazy=True))
 
-def __repre__(self):
+def __repr__(self):
     return "<Tweet: {}>".format(self.text)
+
+#example users but remember they don't tweets
+def insert_example_users():
+    """Example Users"""
+    bill = User(id=1, name="BillGates")
+    ellon = User(id=2, name='ElonMusk')
+    DB.session.add(bill)
+    DB.session.add(ellon)
+    DB.session.commit()
